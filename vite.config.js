9d0c89d72b876fd07b1e039dbbcc6cf24144139a
JhosueGUI <159204953+JhosueGUI@users.noreply.github.com.>
2025-02-27 16:03:40 -0500

    s

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Permite conexiones externas
+    port: process.env.PORT || 8080, // Usa el puerto asignado por Railway
+  }
+});
